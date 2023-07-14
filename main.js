54b1ab3f7893a719b09b3fcf3e6c1b09654602eb
Ariela88 <135212709+Ariela88@users.noreply.github.com>
2023-07-14 09:41:56 +0200

    commmit

---
+++ main.js
@@ -0,0 +1,24 @@
+
+
+
+// const canvasCollection = document.getElementsByClassName('my-canvas')
+// const canvasA = canvasCollection[0]
+
+
+// const canvasCollection2 = document.getElementsByTagName('canvas')
+// const canvasB = canvasCollection2[0]
+
+
+// console.log(canvasA)
+// console.log(canvasB)
+
+
+// const canvasC = document.querySelector('.my-canvas')//Il Query chiama l'elemento con la modalità del CSS e prende il primo che trova
+
+
+const canvas = document.getElementById('canvas1')
+// console.log(canvas.width)
+// console.log(canvas.height)
+
+const ctx = canvas.getContext('2d')
+
